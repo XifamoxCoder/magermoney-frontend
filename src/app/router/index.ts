ae6b6d5595feb042bb05995e50ac066acbfba0b9
XifamoxCoder <pavelmager2006@mail.ru>
2024-06-14 21:12:24 +0300

    added routing

---
+++ src/app/router/index.ts
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import routes from './routes';
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes,
+	scrollBehavior() {
+		return new Promise((resolve) => {
+			resolve({ left: 0, top: 0 });
+		});
+	}
+});
+
+export default router;
